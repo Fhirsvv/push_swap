131b8100fac46ee827539098dbdbd0f20898c443
Fede <federicojose2000@gmail.com>
2023-11-07 23:18:40 +0100

    rotate funtions

--- src/rotate.c
+++ src/rotate.c
@@ -11,3 +11,34 @@
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+int rotate(t_stack **stack)
+{
+    t_stack *aux;
+    t_stack *last;
+
+    aux = *stack;
+    *stack = (*stack)->next;
+    last = ft_lstlast(*stack);
+    aux->next = NULL;
+    last->next = aux;
+}
+
+void    ft_ra(t_stack **stack_a)
+{
+    rotate(stack_a);
+    ft_printf("%s", "ra");
+}
+
+void    ft_rb(t_stack **stack_b)
+{
+    rotate(stack_b);
+    ft_printf("%s", "rb");
+}
+
+void    ft_rr(t_stack **stack_a, t_stack **stack_b)
+{
+    rotate(stack_a);
+    rotate(stack_b);
+    ft_printf("%s", "rr");
+}
