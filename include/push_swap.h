b9db8a49f68b0b011b06aefa53ba50f750e6a3de
Fede <federicojose2000@gmail.com>
2023-11-11 22:25:02 +0100

    added functions prototypes

--- include/push_swap.h
+++ include/push_swap.h
@@ -21,5 +21,22 @@ typedef struct s_stack
     struct s_stack *next;
 }t_stack;
 
-// int	check_args(char **str);
+
 int	check_nb_in_argv(char **str, t_stack *stack_A);
+t_stack	*ft_get_penultimate(t_stack *stack);
+void order_three(t_stack **stack_a);
+void	ft_sa(t_stack **stack_a);
+void	ft_sb(t_stack **stack_b);
+void	ft_ss(t_stack **stack_a, t_stack **stack_b);
+void    ft_ra(t_stack **stack_a);
+void    ft_rb(t_stack **stack_b);
+void    ft_rr(t_stack **stack_a, t_stack **stack_b);
+void    ft_rra(t_stack **stack_a);
+void    ft_rrb(t_stack **stack_b);
+void    ft_rrr(t_stack **stack_a, t_stack **stack_b);
+void    ft_pa(t_stack **stack_a, t_stack **stack_b);
+void    ft_pb(t_stack **stack_a, t_stack **stack_b);
+int	check_nb_in_argv(char **str, t_stack *stack_a);
+int	check_duplicated(t_stack *stack_a);
+int check_and_save(char *str, t_stack *first_node);
+int	check_if_nb(char *str);
