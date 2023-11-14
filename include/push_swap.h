00eeedb2cd7b11fe33dd71e444f8bdc03dabae24
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r14s2.42madrid.com>
2023-11-14 11:27:28 +0100

    changes for save

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:50:44 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/11/13 16:51:14 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/11/14 11:23:02 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ typedef struct s_stack
     struct s_stack *next;
 }t_stack;
 
-
-int	check_nb_in_argv(char **str, t_stack *stack_A);
 t_stack	*ft_get_penultimate(t_stack *stack);
 void order_three(t_stack **stack_a);
 void	ft_sa(t_stack **stack_a);
@@ -36,12 +34,13 @@ void    ft_rrb(t_stack **stack_b);
 void    ft_rrr(t_stack **stack_a, t_stack **stack_b);
 void    ft_pa(t_stack **stack_a, t_stack **stack_b);
 void    ft_pb(t_stack **stack_a, t_stack **stack_b);
-int	check_nb_in_argv(char **str, t_stack *stack_a);
-int	check_duplicated(t_stack *stack_a);
-int check_and_save(char *str, t_stack *first_node);
+t_stack	*check_nb_in_argv(char *str, t_stack **stack_a);
+int	check_duplicated(t_stack **stack_a);
+t_stack *check_and_save(char *str, t_stack **first_node);
 int	check_if_nb(char *str);
 t_stack	*ft_lstnew_ps(int content);
 void	ft_lstadd_back_ps(t_stack **lst, t_stack *new);
 void	ft_lstadd_front_ps(t_stack **lst, t_stack *new);
 t_stack	*ft_lstlast_ps(t_stack *lst);
 int	ft_lstsize_ps(t_stack *lst);
+t_stack	*resort_ags(char **argv, t_stack **stack);
