34d01a1abf112e03ddb83ddf987306f73016c558
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r12s5.42madrid.com>
2023-11-21 13:22:59 +0100

    norminnette

--- src/rotate.c
+++ src/rotate.c
@@ -6,40 +6,39 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:00:10 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/11/14 15:48:57 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/11/21 13:22:39 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-int rotate(t_stack **stack)
+void	rotate(t_stack **stack)
 {
-    t_stack *aux;
-    t_stack *last;
+	t_stack	*aux;
+	t_stack	*last;
 
-    aux = *stack;
-    *stack = (*stack)->next;
-    last = ft_lstlast_ps(*stack);
-    aux->next = NULL;
-    last->next = aux;
-    return(1);
+	aux = *stack;
+	*stack = (*stack)->next;
+	last = ft_lstlast_ps(*stack);
+	aux->next = NULL;
+	last->next = aux;
 }
 
-void    ft_ra(t_stack **stack_a)
+void	ft_ra(t_stack **stack_a)
 {
-    rotate(stack_a);
-    ft_printf("%s\n", "ra");
+	rotate(stack_a);
+	ft_printf("%s\n", "ra");
 }
 
-void    ft_rb(t_stack **stack_b)
+void	ft_rb(t_stack **stack_b)
 {
-    rotate(stack_b);
-    ft_printf("%s\n", "rb");
+	rotate(stack_b);
+	ft_printf("%s\n", "rb");
 }
 
-void    ft_rr(t_stack **stack_a, t_stack **stack_b)
+void	ft_rr(t_stack **stack_a, t_stack **stack_b)
 {
-    rotate(stack_a);
-    rotate(stack_b);
-    ft_printf("%s\n", "rr");
+	rotate(stack_a);
+	rotate(stack_b);
+	ft_printf("%s\n", "rr");
 }
