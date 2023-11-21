a7a7abee3bf0224947ba939cfc1f2177d9bb7985
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r12s5.42madrid.com>
2023-11-21 13:26:19 +0100

    normi

--- src/swap.c
+++ src/swap.c
@@ -6,40 +6,39 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:00:12 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/11/15 15:23:08 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/11/21 13:25:54 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-static int    ft_swap(t_stack **stack)
-{    
-    t_stack *aux_stack;
+static void	ft_swap(t_stack **stack)
+{
+	t_stack	*aux_stack;
 
-    if (ft_lstsize_ps(stack) < 2)
-        return (0);
+	if (ft_lstsize_ps(stack) < 2)
+		return ;
 	aux_stack = *stack;
 	*stack = (*stack)->next;
 	aux_stack->next = (*stack)->next;
 	(*stack)->next = aux_stack;
-    return(1);
 }
 
 void	ft_sa(t_stack **stack_a)
 {
 	ft_swap(stack_a);
-	ft_printf("%s\n","sa");
+	ft_printf("%s\n", "sa");
 }
 
 void	ft_sb(t_stack **stack_b)
 {
 	ft_swap(stack_b);
-	ft_printf("%s\n","sb");
+	ft_printf("%s\n", "sb");
 }
 
 void	ft_ss(t_stack **stack_a, t_stack **stack_b)
 {
 	ft_swap(stack_a);
 	ft_swap(stack_b);
-	ft_printf("%s\n","ss");
+	ft_printf("%s\n", "ss");
 }
