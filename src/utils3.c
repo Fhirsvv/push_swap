227876da8d6a8dbafc0baca65f2ee019c9091656
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r12s5.42madrid.com>
2023-11-21 13:33:43 +0100

    normiok

--- src/utils3.c
+++ src/utils3.c
@@ -6,13 +6,13 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 17:58:09 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/11/19 17:46:09 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/11/21 13:33:25 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-int find_min(t_stack **stack)
+int	find_min(t_stack **stack)
 {
 	int	min;
 
@@ -23,22 +23,20 @@ int find_min(t_stack **stack)
 			min = (*stack)->value;
 		stack = &(*stack)->next;
 	}
-	// ft_printf("find_min:%i\n", min);
 	return (min);
 }
 
-int find_max(t_stack **stack)
+int	find_max(t_stack **stack)
 {
 	int	max;
 
 	max = (*stack)->value;
-	while ((*stack))
+	while (*stack)
 	{
 		if (max < (*stack)->value)
 			max = (*stack)->value;
 		stack = &(*stack)->next;
-	}
-	// ft_printf("find_max:%i\n", max);
+	}	
 	return (max);
 }
 
@@ -47,41 +45,40 @@ int	ft_lstsize_ps(t_stack **stack)
 	int	i;
 
 	i = 0;
-	while ((*stack))
+	while (*stack)
 	{
-		stack = &(*stack) -> next;
+		stack = &(*stack)->next;
 		i++;
 	}
 	return (i);
 }
+
 int	find_pos_index(t_stack **stack, int nb)
 {
-    int pos;    
-	
-	pos = 1;	
-    while ((*stack))
-    {
-        if ((*stack)->index == nb)
-            break;
+	int	pos;
 
-        pos++;
-        stack = &(*stack)->next;		
-    }	
-    return (pos);
+	pos = 1;
+	while (*stack)
+	{
+		if ((*stack)->index == nb)
+			break ;
+		pos++;
+		stack = &(*stack)->next;
+	}
+	return (pos);
 }
 
 int	find_pos_value(t_stack **stack, int nb)
 {
-    int pos;    
-	
-	pos = 1;	
-    while ((stack))
-    {
-        if ((*stack)->value == nb)
-            break;
+	int	pos;
 
-        pos++;
-        stack = &(*stack)->next;		
-    }	
-    return (pos);
+	pos = 1;
+	while (*stack)
+	{
+		if ((*stack)->value == nb)
+			break ;
+		pos++;
+		stack = &(*stack)->next;
+	}
+	return (pos);
 }
