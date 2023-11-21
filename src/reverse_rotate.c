ab4b62b443916990809470ac4aeacc8f3e90f81e
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r12s5.42madrid.com>
2023-11-21 13:21:39 +0100

    normi

--- src/reverse_rotate.c
+++ src/reverse_rotate.c
@@ -6,42 +6,41 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:00:05 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/11/14 15:49:40 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/11/21 13:21:28 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-int reverse_rotate(t_stack **stack)
+void	reverse_rotate(t_stack **stack)
 {
-    t_stack *last;
-    t_stack *aux;
-    t_stack *new_last;
+	t_stack	*last;
+	t_stack	*aux;
+	t_stack	*new_last;
 
-    last = ft_lstlast_ps(*stack);
-    new_last = ft_get_penultimate(*stack);
-    aux = *stack;
-    *stack = last;
-    (*stack)->next = aux;
-    new_last->next = NULL;
-    return (1);
+	last = ft_lstlast_ps(*stack);
+	new_last = ft_get_penultimate(*stack);
+	aux = *stack;
+	*stack = last;
+	(*stack)->next = aux;
+	new_last->next = NULL;
 }
 
-void    ft_rra(t_stack **stack_a)
+void	ft_rra(t_stack **stack_a)
 {
-    reverse_rotate(stack_a);
-    ft_printf("%s\n", "rra");
+	reverse_rotate(stack_a);
+	ft_printf("%s\n", "rra");
 }
 
-void    ft_rrb(t_stack **stack_b)
+void	ft_rrb(t_stack **stack_b)
 {
-    reverse_rotate(stack_b);
-    ft_printf("%s\n", "rrb");
+	reverse_rotate(stack_b);
+	ft_printf("%s\n", "rrb");
 }
 
-void    ft_rrr(t_stack **stack_a, t_stack **stack_b)
+void	ft_rrr(t_stack **stack_a, t_stack **stack_b)
 {
-    ft_rra(stack_a);
-    ft_rrb(stack_b);
-    ft_printf("%s\n", "rrr");
+	ft_rra(stack_a);
+	ft_rrb(stack_b);
+	ft_printf("%s\n", "rrr");
 }
